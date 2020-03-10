b5339fdd114a74d05264545d1fa85ee173c7a5ce
Adrian Kuta <adrian.kuta93@gmail.com>
2020-03-10 13:06:24 +0100

    Header navigation.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,40 +2,43 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogpost':
-      return [...state,
+    case "add_blogpost":
+      return [
+        ...state,
         {
           id: Math.floor(Math.random() * 99999),
           title: `Blog Post #${state.length + 1}`
-        }];
-    case 'delete_blogpost':
+        }
+      ];
+    case "delete_blogpost":
       return state.filter(blogPost => blogPost.id !== action.payload);
-    case 'clear':
+    case "clear":
       return [];
     default:
       return state;
   }
 };
 
-const addBlogPost = (dispatch) => {
+const addBlogPost = dispatch => {
   return () => {
-    dispatch({type: 'add_blogpost'})
+    dispatch({type: "add_blogpost"});
   };
 };
 
-const deleteItem = (dispatch) => {
-  return (id) => {
-    dispatch({type: 'delete_blogpost', payload: id})
-  }
+const deleteItem = dispatch => {
+  return id => {
+    dispatch({type: "delete_blogpost", payload: id});
+  };
 };
 
-const clear = (dispatch) => {
+const clear = dispatch => {
   return () => {
-    dispatch({type: 'clear'})
+    dispatch({type: "clear"});
   };
 };
 
 export const {Context, Provider} = createDataContext(
   blogReducer,
   {addBlogPost, clear, deleteItem},
-  []);
+  []
+);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,29 +1,56 @@
 import React, {useContext} from "react";
 import {Button, FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {Context as BlogContext} from "../context/BlogContext";
-import {MaterialCommunityIcons} from '@expo/vector-icons';
+import {MaterialCommunityIcons, MaterialIcons} from "@expo/vector-icons";
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
   const {state, addBlogPost, deleteItem, clear} = useContext(BlogContext);
 
   function getRenderItem({item}) {
-    return <View style={styles.rowStyle}>
-      <Text style={styles.titleStyle}>{item.title} - {item.id}</Text>
-      <TouchableOpacity onPress={() => deleteItem(item.id)}>
-        <MaterialCommunityIcons name="delete-outline" style={styles.iconStyle}/>
+    return (
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Show", {id: item.id})}
+      >
+        <View style={styles.rowStyle}>
+          <Text style={styles.titleStyle}>
+            {item.title} - {item.id}
+          </Text>
+          <TouchableOpacity onPress={() => deleteItem(item.id)}>
+            <MaterialCommunityIcons
+              name="delete-outline"
+              style={styles.iconStyle}
+            />
+          </TouchableOpacity>
+        </View>
       </TouchableOpacity>
-    </View>
+    );
   }
 
-  return <View>
-    <Button style={styles.buttonStyle} title='Add Blog Post' onPress={addBlogPost}/>
-    <Button style={styles.buttonStyle} title='Clear all' onPress={clear}/>
-    <FlatList
-      data={state}
-      renderItem={getRenderItem}
-      keyExtractor={(item) => item.id}
-    />
-  </View>
+  return (
+    <View>
+      <Button
+        style={styles.buttonStyle}
+        title="Add Blog Post"
+        onPress={addBlogPost}
+      />
+      <Button style={styles.buttonStyle} title="Clear all" onPress={clear}/>
+      <FlatList
+        data={state}
+        renderItem={getRenderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+IndexScreen.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <MaterialIcons name="add" size={30}/>
+      </TouchableOpacity>
+    )
+  };
 };
 
 const styles = StyleSheet.create({
@@ -31,12 +58,12 @@ const styles = StyleSheet.create({
     margin: 10
   },
   rowStyle: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingVertical: 20,
     paddingHorizontal: 10,
     borderTopWidth: 1,
-    borderColor: 'gray'
+    borderColor: "gray"
   },
   iconStyle: {
     fontSize: 24
